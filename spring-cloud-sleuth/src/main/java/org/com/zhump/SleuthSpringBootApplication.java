6fff138f14579a5243fec923c9e90cf8575aa790
zhump <1174435513@qq.com>
2020-07-27 17:12:47 +0800

    新增路由存储方式到数据库

--- spring-cloud-sleuth/src/main/java/org/com/zhump/SleuthSpringBootApplication.java
+++ spring-cloud-sleuth/src/main/java/org/com/zhump/SleuthSpringBootApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import zipkin2.server.internal.EnableZipkinServer;
+import zipkin2.storage.mysql.v1.MySQLStorage;
 
 import javax.sql.DataSource;
 
@@ -16,6 +16,11 @@ public class SleuthSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SleuthSpringBootApplication.class, args);
+    }
+
+    @Bean
+    public MySQLStorage mySQLStorage(DataSource dataSource) {
+        return MySQLStorage.newBuilder().datasource(dataSource).executor(Runnable::run).build();
     }
 
 }
